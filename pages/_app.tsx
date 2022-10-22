96015caf0c66f6a1a6e6c4cbe03ff944c9c7b9b1
kazutoichikawa <74768712+kazutoichikawa@users.noreply.github.com>
2022-10-22 22:13:22 +0900

    graphqlのAPIを叩けるように変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,17 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app'
 const cache = new InMemoryCache();
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
+  uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
   cache,
 });
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  console.log(1, process.env.NEXT_PUBLIC_API_URL)
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/components/PostsList.tsx
@@ -0,0 +1,57 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { useMemo } from "react";
+
+type Post = {
+  id: string;
+  title: string;
+}
+
+type PostsData = {
+  posts: Post[];
+}
+
+type PostsListProps = {}
+
+const PostsList: NextPage<PostsListProps> = () => {
+  const POSTS_QUERY = gql`
+    query {
+      posts {
+        id
+        title
+      }
+    }
+  `
+  const { loading: postsLoading, data: postsData, error: postsError } = useQuery(POSTS_QUERY);
+
+  const postsListElement = useMemo(() => {
+    if (postsLoading) return <div>Loading...</div>
+    if (postsError) return <div>記事の取得に失敗しました</div>
+
+    const elements: JSX.Element[] = [];
+    console.log(postsData)
+    postsData.posts.forEach((post: Post) => {
+      elements.push(
+        <li>
+          <p>タイトル: {post.title}</p>
+        </li>
+      )
+    });
+
+    return (
+      <>{elements}</>
+    )
+  }, [postsLoading, postsData, postsError])
+  return (
+    <div>
+      <ul>
+        {postsListElement}
+      </ul>
+    </div>
+  )
+
+}
+
+export default PostsList;
